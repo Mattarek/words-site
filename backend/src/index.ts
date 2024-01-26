30da20a5a1a4226d16ff9c93ece89263a79e642a
Marcin Barć <marb637@gmail.com>
2024-01-26 21:54:59 +0100

    tsconfig added to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,36 +4,53 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 
-import { createWriteStream } from 'fs';
+import { createWriteStream, promises } from 'fs';
 import { join } from 'path';
 
 import poolDB from './db';
 
 const app: Application = express();
 const port = process.env.PORT || 3001;
-const accessLogStream = createWriteStream(
-    join(__dirname, './logs/access.log'),
-    { flags: 'w' },
-);
+
+
+async function createLogsDirectory() {
+    try {
+        await promises.access(join(__dirname, 'logs'));
+    } catch (error) {
+        // "logs" directory doesn't exist, create it
+        await promises.mkdir(join(__dirname, 'logs'));
+    } finally {
+        const accessLogStream = createWriteStream(
+            join(__dirname, './logs/access.log'),
+            { flags: 'w' }
+        );
+        app.use(
+            morgan(
+                '\nDate: :date[iso] \nIp: :remote-addr :remote-user \nStatus: :http-version :method :url :status :response-time ms',
+                {
+                    stream: accessLogStream,
+                },
+            ),
+        );
+
+    }
+}
+createLogsDirectory()
+
 
 // Middleware
 dotenv.config();
 app.use(express.json());
 app.use(helmet());
-
 app.use(
     cors({
         origin: process.env.FRONTEND_URL,
     }),
 );
-
 app.use(
-    morgan(
-        '\nDate: :date[iso] \nIp: :remote-addr :remote-user \nStatus: :http-version :method :url :status :response-time ms',
-        {
-            stream: accessLogStream,
-        },
-    ),
+    cors({
+        origin: process.env.FRONTEND_URL,
+    }),
 );
 
 const run = async () => {
